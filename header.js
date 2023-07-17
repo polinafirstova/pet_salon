e5ffb778e387fb0c897ee245298f8a334f73c8c1
polinafirstova <112416993+polinafirstova@users.noreply.github.com>
2023-07-17 21:26:29 +0700

    Update header.js

--- header.js
+++ header.js
@@ -26,6 +26,9 @@ button_close.addEventListener("click", closeMenu);
 function openMenu() {
     menu.classList.remove("hide");
     menu.style.opacity = "1";
+    menu.style.width = "100%";
+    menu.style.height = "100%";
+    menu.style.background = "rgba(72, 72, 72, 0.8)";
     button_menu.style.display = "none";
     button_close.style.display = "block";
     link.style.display = "flex";
@@ -34,6 +37,8 @@ function openMenu() {
 
 function closeMenu() {
     menu.classList.add("hide");
+    menu.style.width = "0";
+    menu.style.height = "0";
     menu.style.opacity = "0";
     button_close.style.display = "none";
     button_menu.style.display = "block";
@@ -42,8 +47,10 @@ function closeMenu() {
 }
 
 window.onclick = function (event) {
-    if (event.target == menu) {
+    if (event.target === menu) {
         menu.classList.add("hide");
+        menu.style.width = "0";
+        menu.style.height = "0";
         menu.style.opacity = "0";
         button_close.style.display = "none";
         button_menu.style.display = "block";
