16c974f9c49786597f1a69daa427f432fdee95c1
polinafirstova <112416993+polinafirstova@users.noreply.github.com>
2023-07-17 21:06:04 +0700

    Update header.js

--- header.js
+++ header.js
@@ -1,19 +1,3 @@
-let lastScroll = 0;
-const defaultOffset = 100;
-const header = document.getElementsByClassName("header")[0];
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY > lastScroll && !header.classList.contains("hide") && window.scrollY > defaultOffset) {
-        header.classList.add("hide");
-        console.log('down');
-    } else if (window.scrollY < lastScroll && header.classList.contains("hide")) {
-        header.classList.remove("hide");
-        console.log('up');
-    }
-
-    lastScroll = window.scrollY;
-});
-
 const button_menu = document.getElementsByClassName("menu")[0];
 const button_close = document.getElementsByClassName("close")[0];
 const link = document.getElementsByClassName("navigation_list_2")[0];
@@ -21,10 +5,26 @@ const home = document.querySelector("a.home");
 
 const menu = document.getElementsByClassName("for_phone")[0];
 
+let lastScroll = 0;
+const defaultOffset = 100;
+const header = document.getElementsByClassName("header")[0];
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > lastScroll && !header.classList.contains("hide") && window.scrollY > defaultOffset) {
+        header.classList.add("hide");
+    } else if (window.scrollY < lastScroll && header.classList.contains("hide")) {
+        header.classList.remove("hide");
+    }
+
+    lastScroll = window.scrollY;
+});
+
+
 button_menu.addEventListener("click", openMenu);
 button_close.addEventListener("click", closeMenu);
 
 function openMenu() {
+    menu.classList.remove("hide");
     menu.style.opacity = "1";
     button_menu.style.display = "none";
     button_close.style.display = "block";
@@ -33,6 +33,7 @@ function openMenu() {
 }
 
 function closeMenu() {
+    menu.classList.add("hide");
     menu.style.opacity = "0";
     button_close.style.display = "none";
     button_menu.style.display = "block";
@@ -42,6 +43,7 @@ function closeMenu() {
 
 window.onclick = function (event) {
     if (event.target == menu) {
+        menu.classList.add("hide");
         menu.style.opacity = "0";
         button_close.style.display = "none";
         button_menu.style.display = "block";
